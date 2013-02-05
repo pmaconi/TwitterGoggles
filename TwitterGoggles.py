315cf0d54b3ada707ecb32f334f63774ec79fb97
Phil Maconi <pmaconi@gmail.com>
2013-02-05 01:33:55 -0800

    Tweet parsing and insertion

--- TwitterGoggles.py
+++ TwitterGoggles.py
@@ -1,9 +1,13 @@
-import argparse, configparser, math, mysql.connector as sql, requests, sys, time
+import argparse, collections, configparser, json, math, mysql.connector as sql, requests, sys, time
 from datetime import datetime
 from mysql.connector import errorcode
 from requests import HTTPError
 from requests import ConnectionError
 from requests_oauthlib import OAuth1
+
+def verbose(info) :
+	if args.verbose:
+		print(info)
 
 def connect() :
 	# Connect to MySQL using config entries
@@ -32,18 +36,17 @@ def getJobs(conn) :
 
 	cursor.execute(query,[args.head])
 	return cursor
-	
 
 def search(query, since_id, oauth) :
-	full_query = "&".join([query,"since_id=" + since_id, "rpp=100", "include_entities"])
+	full_query = "&".join([query,"since_id=" + since_id, "rpp=100", "include_entities=1"])
 	verbose("Query: " + full_query)
 
 	attempt = 1
 	while attempt <= 3 :
 		try :
 			r = requests.get("https://api.twitter.com/1.1/search/tweets.json?" + full_query, auth=oauth)
-			return r.json
-			
+			return json.loads(r.text)
+
 		except (ConnectionError, HTTPError) as err :
 			sleep_time = 2**(attempt - 1)
 			verbose("Connection attempt " + str(attempt) + " failed. "
@@ -54,17 +57,50 @@ def search(query, since_id, oauth) :
 	print("***** Error: Unable to query Twitter. Terminating.")
 	sys.exit(1)
 
-def getOAuth(consumer_key, consumer_secret, access_token, access_token_secret) : 
-	oauth = OAuth1(client_key=consumer_key,
-				client_secret=consumer_secret,
-				resource_owner_key=access_token,
-				resource_owner_secret=access_token_secret)
+def addTweet(conn, job_id, tweet) :
+	cursor = conn.cursor()
 
-	return oauth
+	prefix = "INSERT INTO tweet (tweet_id_str, job_id, created_at, text, from_user, from_user_id_str, " \
+		"from_user_name, to_user, to_user_id_str, to_user_name, source, iso_language"
+	suffix = ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
+	values = [
+		tweet["id_str"],
+		job_id,
+		datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y').strftime('%Y-%m-%d %H:%M:%S'),
+		tweet["text"],
+		tweet["user"]["id"],
+		tweet["user"]["id_str"],
+		tweet["user"]["screen_name"],
+		tweet["in_reply_to_user_id"],
+		tweet["in_reply_to_user_id_str"],
+		tweet["in_reply_to_screen_name"],
+		tweet["source"],
+		tweet["metadata"]["iso_language_code"]
+	]
 
-def verbose(info) :
-	if args.verbose:
-		print(info)
+	if tweet['geo'] is not None and tweet['geo']['type'] == "Point" :		
+		prefix = prefix + ", location_geo, location_geo_0, location_geo_1"
+		suffix = suffix + ", %s, %s, %s"
+		values.extend([
+			"point(" + tweet["geo"]["coordinates"][0] + "," + tweet["geo"]["coordinates"][1] + ")",
+			tweet["geo"]["coordinates"][0],
+			tweet["geo"]["coordinates"][1]
+		])
+
+	suffix = suffix + ")"
+	query = (prefix + suffix)
+	
+	try :
+		cursor.execute(query, values)
+		conn.commit()
+		return True
+	except sql.Error as err :
+		verbose(">>>> Warning: Could not add Tweet: " + str(err))
+		# Convert unprintable utf8 string to ascii bytes and decode to string
+		verbose("     Query: " + cursor.statement.encode("ascii", "ignore").decode())
+		return False
+	finally :
+		cursor.close()
 
 if __name__ == '__main__' :
 	# Handle command line arguments
@@ -108,14 +144,31 @@ if __name__ == '__main__' :
 			
 			print("+++++ Job ID:", job_id, "\tDescription:", description, "\tQuery:", query)
 
-			oauth = getOAuth(consumer_key, consumer_secret, access_token, access_token_secret)
+			oauth = OAuth1(client_key=consumer_key,
+						client_secret=consumer_secret,
+						resource_owner_key=access_token,
+						resource_owner_secret=access_token_secret)
 			
-			search(query, since_id_str.decode('utf8'), oauth)
-		#search("test")
+			# Get the Tweets
+			results = search(query, since_id_str.decode('utf8'), oauth)
+
+			total_results = results["search_metadata"]["count"]
+			max_id = results["search_metadata"]["max_id"]
+			verbose("Max ID: " + str(max_id))
+
+			tweets = collections.deque()
+			tweets.extend(results["statuses"])
+
+			while tweets :
+				tweet = tweets.popleft()
+
+				success = addTweet(conn, job_id, tweet)
+
+
 	except sql.Error as err :
-			print(err)
-			print("Terminating.")
-			sys.exit(1)
+		print(err)
+		print("Terminating.")
+		sys.exit(1)
 	else :
 		conn.close()
 	finally :
